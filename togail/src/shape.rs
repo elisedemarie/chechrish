e1acf12d2fd0be66d4d56130c522cce6584f2fdd
elise <135685125+elisedemarie@users.noreply.github.com>
2026-06-13 17:40:43 +1000

    Add rotations.

--- togail/src/shape.rs
+++ togail/src/shape.rs
@@ -1,4 +1,4 @@
-use crate::ROWS;
+use crate::COLS;
 use alloc::vec::Vec;
 use alloc::vec;
 
@@ -10,14 +10,10 @@ pub struct Position {
 impl Position {
     pub fn start_position() -> Self {
         Self {
-            x: (ROWS / 2) as isize,
+            x: 0,
             y: 0,
         }
     }
-
-    pub fn new(x: usize, y: usize) -> Self {
-        Self {x, y}
-    } 
 }
 
 pub enum ShapeType {
@@ -28,6 +24,55 @@ pub enum ShapeType {
     Z,
     J,
     L,
+}
+
+impl ShapeType {
+    pub fn shape_cells(&self) -> Vec<Position> {
+        match self {
+            Self::I => vec![
+                Position {x: 0, y: 0},
+                Position {x: 0, y: 1},
+                Position {x: 0, y: 2},
+                Position {x: 0, y: 3},
+            ],
+            Self::T => vec![
+                Position {x: 0, y: 0},
+                Position {x: 1, y: 0},
+                Position {x: 2, y: 0},
+                Position {x: 1, y: 1},
+            ],
+            Self::O => vec![
+                Position {x: 0, y: 0},
+                Position {x: 1, y: 0},
+                Position {x: 0, y: 1},
+                Position {x: 1, y: 1},
+            ],
+            Self::Z => vec![
+                Position {x: 0, y: 0},
+                Position {x: 1, y: 0},
+                Position {x: 1, y: 1},
+                Position {x: 2, y: 1},
+            ],
+            Self::S => vec![
+                Position {x: 1, y: 0},
+                Position {x: 2, y: 0},
+                Position {x: 0, y: 1},
+                Position {x: 1, y: 1},
+            ],
+            Self::J => vec![
+                Position {x: 1, y: 0},
+                Position {x: 1, y: 1},
+                Position {x: 1, y: 2},
+                Position {x: 0, y: 2},
+            ],
+            Self::L => vec![
+                Position {x: 0, y: 0},
+                Position {x: 0, y: 1},
+                Position {x: 0, y: 2},
+                Position {x: 1, y: 2},
+            ],
+        }
+    }
 }
 
 pub enum Orientation {
@@ -53,49 +98,22 @@ impl Shape {
     }
 
     pub fn get_cells(&self) -> Vec<Position> {
-        match self.shape_type {
-            ShapeType::I => vec![
-                Position {x: 0, y: 0},
-                Position {x: 0, y: 1},
-                Position {x: 0, y: 2},
-                Position {x: 0, y: 3},
-            ],
-            ShapeType::T => vec![
-                Position {x: 0, y: 0},
-                Position {x: 1, y: 0},
-                Position {x: 2, y: 0},
-                Position {x: 1, y: 1},
-            ],
-            ShapeType::O => vec![
-                Position {x: 0, y: 0},
-                Position {x: 1, y: 0},
-                Position {x: 0, y: 1},
-                Position {x: 1, y: 1},
-            ],
-            ShapeType::Z => vec![
-                Position {x: 0, y: 0},
-                Position {x: 1, y: 0},
-                Position {x: 1, y: 1},
-                Position {x: 2, y: 1},
-            ],
-            ShapeType::S => vec![
-                Position {x: 1, y: 0},
-                Position {x: 2, y: 0},
-                Position {x: 0, y: 1},
-                Position {x: 1, y: 1},
-            ],
-            ShapeType::J => vec![
-                Position {x: 1, y: 0},
-                Position {x: 1, y: 1},
-                Position {x: 1, y: 2},
-                Position {x: 0, y: 2},
-            ],
-            ShapeType::L => vec![
-                Position {x: 0, y: 0},
-                Position {x: 0, y: 1},
-                Position {x: 0, y: 2},
-                Position {x: 1, y: 2},
-            ],
+        let shape_sells = self.shape_type.shape_cells();
+        match self.orientation {
+            Orientation::North => shape_sells,
+            Orientation::East => rotate_shape(shape_sells),
+            Orientation::South => rotate_shape(rotate_shape(shape_sells)),
+            Orientation::West => rotate_shape(rotate_shape(rotate_shape(shape_sells)))
         }
     } 
+
+}
+
+fn rotate_shape(positions: Vec<Position>) -> Vec<Position> {
+    positions.iter().map(|pos: &Position| {
+        Position {
+            x: pos.y,
+            y: -pos.x
+        }
+    }).collect()
 }
